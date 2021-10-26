14cc536b5565142c95e3f638d6f92157613e2ccd
JayoonKoo <rnwkdbs12@gmail.com>
2021-10-26 16:20:48 +0900

    feat: 구구단 게임 완성
    기존에 Class 로 만들었던 것을 Hooks를 사용하여 함수형 컴포넌트로 변경

---
+++ lecture2/hooks.js
@@ -0,0 +1,45 @@
+const { useState, useRef } = React;
+
+function Hooks() {
+  const [first, setFirst] = useState(Math.ceil(Math.random() * 9));
+  const [second, setSecond] = useState(Math.ceil(Math.random() * 9));
+  const [value, setValue] = useState("");
+  const [result, setResult] = useState("");
+	const useInput = useRef(null);
+
+	const isRight = first * second === Number(value);
+
+	const setRandom =  () => {
+		setFirst(Math.ceil(Math.random() * 9))
+		setSecond(Math.ceil(Math.random() * 9))
+	}
+
+	const handleChange = (e) => setValue(e.target.value);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (isRight) {
+			setResult('정답' + value);
+			setRandom();
+			setValue('');
+		} else {
+			setResult('땡');
+		}
+		useInput.current.focus();
+	}
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="userInput">
+          {first} 곱하기 {second} 는 ?
+        </label>
+        <input ref={useInput} type="number" id="userInput" value={value} onChange={handleChange} />
+				<input type="submit" value="입력 !" />
+      </form>
+			<div>{result}</div>
+    </>
+  );
+}
+
+ReactDOM.render(<Hooks />, document.querySelector("#root"));
